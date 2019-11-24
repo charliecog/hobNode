e0bd1f703ec931ab3ae8cd601f022fde3b113047
charliecog <charlie.coggans@mayden.co.uk>
2019-11-24 18:24:46 +0000

    validation: checks length of doc array

--- Controllers/BiscuitController.js
+++ Controllers/BiscuitController.js
@@ -1,10 +1,20 @@
 const DbService = require('../Services/DbService');
 const BiscuitService = require('../Services/BiscuitService');
+const createJsonResponse = require('../Services/JsonResponseService');
 
 let getAllBiscuits = (req, res) => {
     DbService.connectToDB((db)=>{
         BiscuitService.getAllBiscuits(db,(documents) => {
-            res.json(documents)
+            if(documents.length == 0){
+                let ApiResponse = createJsonResponse.unsuccessful()
+                ApiResponse.message = 'Could not retrieve biscuits'
+                res.json(ApiResponse)
+            } else {
+                let ApiResponse = createJsonResponse.successful()
+                ApiResponse.message = 'Biscuits retrieved'
+                ApiResponse.data = documents
+                res.json(ApiResponse)
+            }
         })
     })
 }

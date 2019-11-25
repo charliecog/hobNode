ee438c292faee8eccb0b986055067d95c00a76cc
charliecog <charlie.coggans@mayden.co.uk>
2019-11-25 19:38:09 +0000

    added values to object property tests

--- __test__/Services/JsonResponseService.test.js
+++ __test__/Services/JsonResponseService.test.js
@@ -14,23 +14,27 @@ describe('Successful Json Response', () => {
     })
 
     test('return value success property should be a boolean', () => {
-        let response = createJsonResponse.successful()
+        let response = createJsonResponse.successful();
         expect(typeof response.success).toBe('boolean');
+        expect(response).toHaveProperty('success', true);
     })
 
     test('return value message property should be a string', () => {
-        let response = createJsonResponse.successful()
+        let response = createJsonResponse.successful();
         expect(typeof response.message).toBe('string');
+        expect(response).toHaveProperty('message', '');
     })
 
     test('return value status property should be a number', () => {
-        let response = createJsonResponse.successful()
+        let response = createJsonResponse.successful();
         expect(typeof response.status).toBe('number');
+        expect(response).toHaveProperty('status', 200);
     })
 
     test('return value data property should be an array', () => {
-        let response = createJsonResponse.successful()
+        let response = createJsonResponse.successful();
         expect(Array.isArray(response.data)).toBe(true);
+        expect(response).toHaveProperty('data', []);
     })
 
 })
@@ -49,23 +53,27 @@ describe('Unsuccessful Json Response', () => {
     })
 
     test('return value success property should be a boolean', () => {
-        let response = createJsonResponse.unsuccessful()
+        let response = createJsonResponse.unsuccessful();
         expect(typeof response.success).toBe('boolean');
+        expect(response).toHaveProperty('success', false);
     })
 
     test('return value message property should be a string', () => {
-        let response = createJsonResponse.unsuccessful()
+        let response = createJsonResponse.unsuccessful();
         expect(typeof response.message).toBe('string');
+        expect(response).toHaveProperty('message', '');
     })
 
     test('return value status property should be a number', () => {
-        let response = createJsonResponse.unsuccessful()
+        let response = createJsonResponse.unsuccessful();
         expect(typeof response.status).toBe('number');
+        expect(response).toHaveProperty('status', 404);
     })
 
     test('return value data property should be an array', () => {
-        let response = createJsonResponse.unsuccessful()
+        let response = createJsonResponse.unsuccessful();
         expect(Array.isArray(response.data)).toBe(true);
+        expect(response).toHaveProperty('data', []);
     })
 
 })

0a7fd3b5e3ff287e88fdb59ca49c74fe7540f82f
charliecog <charlie.coggans@mayden.co.uk>
2019-11-26 21:02:14 +0000

    abstracted out app listener to start.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
 const cors = require('cors');
 const routes = require('./Config/routes');
 
@@ -8,4 +7,4 @@ app.use(cors());
 
 routes(app)
 
-app.listen(PORT, ()=> console.log(`App is now running on port ${PORT}`))
+module.exports = app
---
+++ start.js
@@ -0,0 +1,4 @@
+const app = require('./index');
+const PORT = 8000;
+
+app.listen(PORT, ()=> console.log(`App is now running on port ${PORT}`))
